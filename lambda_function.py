f3e65e07e128aa216eaa8674f59f126080b0efd0
MUHAMMAD FAIZ MOHD ZAKI <muhammadfaiz.zaki@gmail.com>
2017-11-27 10:59:19 +0800

    Added feature to get time until next prayer

--- lambda_function.py
+++ lambda_function.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 import urllib2
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
 
 API_BASE="https://waktu-solat-api.herokuapp.com/api/v1/prayer_times.json?negeri=kedah&zon=kuala%20muda"
 
 def lambda_handler(event, context):
     if (event["session"]["application"]["applicationId"] !=
-            "amzn1.ask.skill.53bc8388-xxxx-xxxe-82a4-xxxxxxxxxbe8"):
+            "amzn1.ask.skill.53bc8388-735f-xxxx-82a4-xxxxxxxxxx"):
         raise ValueError("Invalid Application ID")
 
     if event["session"]["new"]:
@@ -86,38 +86,6 @@ def get_all_prayer_times():
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
 
-def get_prayer_time(intent):
-    session_attributes = {}
-    card_title = "Prayer Time"
-    speech_output = "I'm not sure which prayer you wanted the time for. Please try again"
-    reprompt_text = "I'm not sure which prayer you wanted the time for. Try asking Asar for example."
-    should_end_session = False
-
-    response = urllib2.urlopen(API_BASE)
-    waktu_solat = json.load(response)
-
-    if "Prayer" in intent["slots"]:
-        prayer_name = intent["slots"]["Prayer"]["value"]
-        if prayer_name == 'Subuh':
-            prayer_name = 'Dawn'
-        if prayer_name == 'Isyak':
-            prayer_name = 'Night'
-        prayer_code = get_prayer_code(prayer_name.lower())
-
-        if (prayer_code != 99):
-            now = datetime.strptime(datetime.now().strftime('%H:%M'), '%H:%M')
-            prayer_time = datetime.strptime(waktu_solat['data']['zon'][0]['waktu_solat'][prayer_code]['time'], '%H:%M')
-            countdown = prayer_time - now
-            minutes = countdown.seconds/60
-            h,m = divmod(minutes, 60)
-            card_title = "Time remaining until " + prayer_name.title() + " prayer"
-            speech_output = "You have " + h + " hours and " + m " until " prayer_name.title() + " prayer."
-
-            reprompt_text = ""
-
-    return build_response(session_attributes, build_speechlet_response(
-        card_title, speech_output, reprompt_text, should_end_session))
-
 def get_time_remaining(intent):
     session_attributes = {}
     card_title = "Time Remaining Until Next Prayer"
@@ -129,6 +97,38 @@ def get_time_remaining(intent):
     waktu_solat = json.load(response)
 
     if "PrayerTime" in intent["slots"]:
+        prayer_name = intent["slots"]["PrayerTime"]["value"]
+        if prayer_name == 'Subuh':
+            prayer_name = 'Dawn'
+        if prayer_name == 'Isyak':
+            prayer_name = 'Night'
+        prayer_code = get_prayer_code(prayer_name.lower())
+
+        if (prayer_code != 99):
+            now = datetime.strptime(datetime.utcnow().strftime('%H:%M'), '%H:%M') + timedelta(hours=8)
+            prayer_time = datetime.strptime(waktu_solat['data']['zon'][0]['waktu_solat'][prayer_code]['time'], '%H:%M')
+            countdown = prayer_time - now
+            m,s = divmod(countdown.seconds, 60)
+            h,m = divmod(m, 60)
+            card_title = "Time remaining until " + prayer_name.title() + " prayer"
+            speech_output = "You have " + str(h) + " hours and " + str(m) + " minutes until " + prayer_name.title() + " prayer."
+
+            reprompt_text = ""
+
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+def get_prayer_time(intent):
+    session_attributes = {}
+    card_title = "Prayer Time"
+    speech_output = "I'm not sure which prayer you wanted the time for. Please try again"
+    reprompt_text = "I'm not sure which prayer you wanted the time for. Try asking Asar for example."
+    should_end_session = False
+
+    response = urllib2.urlopen(API_BASE)
+    waktu_solat = json.load(response)
+
+    if "Prayer" in intent["slots"]:
         prayer_name = intent["slots"]["Prayer"]["value"]
         if prayer_name == 'Subuh':
             prayer_name = 'Dawn'
